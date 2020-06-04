8510026eeed2444596874ce7d53cfbe286cf3e4c
Björn Wedell <bjorn.wedell@actia.se>
2020-06-04 22:52:03 +0200

    Reformat separated by epics

--- app/html_renderer.py
+++ app/html_renderer.py
@@ -1,8 +1,28 @@
+import os
 def generate_page(structure, user, fromDateString, toDateString, totalTimeSpent):
-    issuesHtml = '<table>'
-    for issue in structure:
+    jira_url = os.environ.get('JIRA_URL', "no.url.given")
+    epics = set(map(lambda issue: issue["epic"], structure))
+    issuesHtml = ""
+    for epic in epics:
+        if not epic:
+            continue
+        spentOnEpic = 0
+        issuesHtml += '<hr>'
+        issuesHtml += f'<p><b><a href="{jira_url}/browse/{epic}">{epic}</a>:</b></p>'
+        issuesHtml += '<table>'
+        for issue in list(filter(lambda issue: issue['epic']==epic,structure)):
+            issuesHtml += '<tr>'
+            issuesHtml += f'<td>{issue["summary"]}</td>'
+            issuesHtml += f'<td>{issue["hours_spent"]} hours</td>'
+            issuesHtml += '</tr>'
+            spentOnEpic += issue["hours_spent"]
+        issuesHtml += '</table>'
+        issuesHtml += f'<p><b>Total epic hours:</b> {spentOnEpic}</p>'
+    issuesHtml += '<hr>'
+    issuesHtml += f'<p><b>Issues outside epics:</b></p>'
+    issuesHtml += '<table>'
+    for issue in list(filter(lambda issue: not issue['epic'],structure)):
         issuesHtml += '<tr>'
-        issuesHtml += f'<td>{issue["epic"]}</td>'
         issuesHtml += f'<td>{issue["summary"]}</td>'
         issuesHtml += f'<td>{issue["hours_spent"]} hours</td>'
         issuesHtml += '</tr>'
